359ea839dcba1b093fbfa0f4a6d57f8af10d75df
Sovandara UOM <Sovandara.Uom@jtrustroyal.com>
2022-04-08 14:07:15 +0700

    Initial Project

--- lib/cdk-static-web-stack.ts
+++ lib/cdk-static-web-stack.ts
@@ -1,16 +1,34 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class CdkStaticWebStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      versioned: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStaticWebQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+      },
+      defaultRootObject: "index.html",
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./source/dist")],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    new CfnOutput(this, "DistributionDomainName", {
+      value: distribution.domainName,
+    });
   }
 }
